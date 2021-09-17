79fe41f37a53a479cf961c52a513d4e68b395631
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-09-17 22:53:27 +0600

    Added lecture02

---
+++ lecture02/conditions.py
@@ -0,0 +1,8 @@
+num = int(input("Take a number: "))
+
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero")
---
+++ lecture02/sequeces.py
@@ -0,0 +1,11 @@
+from typing import OrderedDict, Sequence
+
+
+# Mutable and Ordered
+# Mutable means can be changed the Sequence
+# Orderd means can not be changed the sequence because order matters
+
+name = "Farjana"
+print(name[0])
+print(name[6])
+print(name)
